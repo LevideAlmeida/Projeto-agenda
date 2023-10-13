377fbfe7a63da7c25af089a2b30fc4e8b441bd1e
Levi <thegood878@gmail.com>
2023-10-13 19:56:19 -0300

    preparations

--- contact/models.py
+++ contact/models.py
@@ -11,12 +11,12 @@ from django.utils import timezone
 # picture (image)
 
 
-class contact(models.Model):
+class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=255)
-    create_date = models.DateTimeField(default=timezone.now, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+    created_date = models.DateTimeField(default=timezone.now, blank=True)
     description = models.TextField(blank=True)
 
     def __str__(self) -> str:
