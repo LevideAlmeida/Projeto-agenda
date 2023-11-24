9c442ee13828c36cd86f171af3a8663cc7ec84ab
Levi <thegood878@gmail.com>
2023-11-24 19:40:27 -0300

    user creation form

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 
 def register(request):
     form = RegisterForm()
 
+    # messages.info(request, "info")
+    # messages.warning(request, "warning")
+    # messages.success(request, "success")
+    # messages.error(request, "error")
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Usuario salvo")
+            return redirect("contact:index")
 
     context = {
         "site_title": "Create - User",
